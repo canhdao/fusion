64f3934ea6118f6e053e2f58d6aba570f5409ce4
canhdao <daongoccanh@gmail.com>
2018-09-28 16:39:49 +0700

    - map 2

---
+++ Assets/GSGameplay/Map/2/SCR_Background2.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_Background2 : MonoBehaviour {
+	void Start() {
+		for (int i = 0; i < transform.childCount; i++) {
+			Transform child = transform.GetChild(i);
+			child.position = new Vector3(child.position.x, child.position.y, child.position.y);
+		}
+	}
+}
