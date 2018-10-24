3e574cfa1e12f65ad3af1292cbe68d4f29350e31
canhdao <daongoccanh@gmail.com>
2018-10-24 18:36:39 +0700

    - load zombie shop from config

--- Assets/GSGameplay/ZombieShop/SCR_ZombieShop.cs
+++ Assets/GSGameplay/ZombieShop/SCR_ZombieShop.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class SCR_ZombieShop : MonoBehaviour {
+	public Text[] zombieNames;
+	public Text[] zombieProductionRates;
 	public Text[] zombiePrices;
 
 	void Start() {
 		for (int i = 0; i < zombiePrices.Length; i++) {
+			string productionRate = "PRODUCE " + SCR_Config.ZOMBIE_INFO[i].productionRate.ToString() + "/s";
+			
+			zombieNames[i].text = SCR_Config.ZOMBIE_INFO[i].name.ToUpper();
+			zombieProductionRates[i].text = productionRate;
 			zombiePrices[i].text = SCR_Config.ZOMBIE_INFO[i].price.ToString();
 		}
 	}
