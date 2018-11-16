48789bc0ee646051c758ab58c47b87f97bc4b370
canhdao <daongoccanh@gmail.com>
2018-11-16 11:12:01 +0700

    - zombie names

--- Assets/SCR_Config.cs
+++ Assets/SCR_Config.cs
@@ -21,21 +21,21 @@ public class SCR_Config {
 	public const int PLUS_BRAIN_AMOUNT	= 10;
 
 	public static ZombieInfo[] ZOMBIE_INFO = new ZombieInfo[] {
-		new ZombieInfo("Nor-02",   "Hobos Corporation first experience sample in reviving dead people ", 0.1f, 1),
-		new ZombieInfo("Bro-03",   "Sample of zombie's binary fission abilities", 0.2f, 2),
-		new ZombieInfo("USA-002",  "Reviving American football player", 0.3f, 3),
-		new ZombieInfo("Nor-007",  "Hobos corporation's failed experiment sample", 0.4f, 4),
-		new ZombieInfo("Baby-02",  "Experiment code 02: a giant kid", 0.5f, 5),
-		new ZombieInfo("Liz-001",  "Experiment code 001: Human - lizard hybrid", 0.6f, 6),
-		new ZombieInfo("Talpa-01", "Experiment  no 01: Human - European mole hybrid", 0.7f, 7),
-		new ZombieInfo("Bikini",   "Hybrid experiment between man and woman no 000", 0.8f, 8),
-		new ZombieInfo("Ryan",     "Hybrid between a bodybuilder and a child", 0.9f, 9),
-		new ZombieInfo("Canh",     "First experiment creating the first giant zombie", 1.0f, 10),
-		new ZombieInfo("G-003",    "Hobos corporation has tried to create the first super zombie; this is the first experiment", 1.1f, 11),
-		new ZombieInfo("GA-007",   "With ambition to dominate the world, Hobos Group continue to experiment the next super zombie generation. ", 1.2f, 12),
-		new ZombieInfo("GI-001",   "Power is absolute", 1.3f, 13),
-		new ZombieInfo("G-005",    "Show the power of the Almighty ", 1.4f, 14),
-		new ZombieInfo("GH-002",   "Bring the power of an ancient God. ", 1.5f, 15),
+		new ZombieInfo("Crawlie", "Hobos Corporation first experience sample in reviving dead people", 0.1f, 1),
+		new ZombieInfo("Siblings", "Sample of zombie's binary fission abilities", 0.2f, 2),
+		new ZombieInfo("Gangstar", "Reviving American football player", 0.3f, 3),
+		new ZombieInfo("Psycho", "Hobos corporation's failed experiment sample", 0.4f, 4),
+		new ZombieInfo("Fatbabe", "Experiment code 02: a giant kid", 0.5f, 5),
+		new ZombieInfo("Yogaboy", "Experiment code 001: Human - lizard hybrid", 0.6f, 6),
+		new ZombieInfo("Monkey", "Experiment  no 01: Human - European mole hybrid", 0.7f, 7),
+		new ZombieInfo("Longbody", "Hybrid experiment between man and woman no 000", 0.8f, 8),
+		new ZombieInfo("Gymmer", "Hybrid between a bodybuilder and a child", 0.9f, 9),
+		new ZombieInfo("Titan", "First experiment creating the first giant zombie", 1.0f, 10),
+		new ZombieInfo("Jombie", "Hobos corporation has tried to create the first super zombie", 1.1f, 11),
+		new ZombieInfo("Buddie", "With ambition to dominate the world, Hobos Group continue to experiment the next super zombie generation", 1.2f, 12),
+		new ZombieInfo("Shivie", "Power is absolute", 1.3f, 13),
+		new ZombieInfo("Zesie", "Show the power of the Almighty", 1.4f, 14),
+		new ZombieInfo("Anubie", "Bring the power of an ancient God", 1.5f, 15),
 	};
 	
 	public const float TOMB_SPAWN_INTERVAL = 10.0f;
